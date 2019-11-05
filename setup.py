3aacdd8aef9c974565eaf426c7c6882c3c017ed2
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2019-11-05 20:25:30 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name =  'netdynflow',
     description = 'A package to study complex networks based on their temporal Dynamic Communicability and Flow.',
     url =  'https://github.com/gorkazl/NetDynFlow',
-    version =  '1.0.0b2',
+    version =  '2.0.0',
     license =  'Apache License 2.0',
 
     author =  'Gorka Zamora-Lopez, Matthieu Gilson, Nikos Kouvaris',
