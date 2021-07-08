49697bfca5c41e6a88c8b64a8dd590bda42c9b56
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2021-07-08 19:48:35 +0200

    Function RandomiseWeightedNetwork() added

--- netdynflow/netmodels.py
+++ netdynflow/netmodels.py
@@ -49,6 +49,7 @@ from __future__ import division, print_function
 import numpy as np
 import numpy.linalg
 import scipy.linalg
+from numba import jit
 # Import GAlib for graph analysis and graph generation tools
 ### ACHTUNG!! For now I am importing GAlib but, for the long run, we must
 ### decide whether we want GAlib as a dependency of NetDynFlow, or we
@@ -79,18 +80,23 @@ def ShuffleLinkWeights(con):
     ----------
     con : ndarray
         Adjacency matrix of the (weighted) network.
+
     Returns
     -------
     rewmatrix : ndarray
         A connectivity matrix with links between same nodes as 'con' but the
         link weights shuffled.
+
     """
     # 0) SECURITY CHECKS
     if not type(con) == numpy.ndarray:
         raise TypeError('Please enter the connectivity matrix as a numpy array.')
+    con_shape = np.shape(con)
+    if (len(con_shape) != 2) or (con_shape[0] != con_shape[1]):
+        raise ValueError("Input not aligned. 'con' should be a 2D array of shape (N x N).")
 
     # 1) EXTRACT THE CONSTRAINTS FROM THE con MATRIX
-    N = len(con)
+    N = con_shape[0]
     nzidx = con.nonzero()
     weights = con[nzidx]
 
@@ -101,6 +107,105 @@ def ShuffleLinkWeights(con):
 
     return rewmatrix
 
+@jit
+def RandomiseWeightedNetwork(con):
+    """
+    Randomises a connectivity matrix and its weights.
+
+    Returns a random connectivity matrix with the same number of links and
+    same link weights as the input matrix 'con'. Therefore, both the total
+    weight and the link weight distribution are conserved but the input/output
+    degrees and node strengths are not.
+
+    The function identifies some properties of 'con' in order to conserve
+    elementary properties of 'con'. For example:
+    1) The resulting random weighted network will only contain self-connections
+    (non-zero diagonal entries) if 'con' contains self-connections.
+    2) If 'con' is an unweighted adjacency matrix (directed or undirected), the
+    result is an Erdos-Renyi-type random graph (directed or undirected),
+    of same size and number of links as 'con'.
+    3) If 'con' is an undirected network but contains asymmetric link weights,
+    the result will be an undirected random graph with asymmetric weights.
+    4) If 'con is a directed weighted network, the result will be a directed and
+    weighted network. In this case, weights cannot be symmetric.
+
+    Parameters
+    ----------
+    con : ndarray
+        Adjacency matrix of the (weighted) network.
+
+    Returns
+    -------
+    rewcon : ndarray
+        A connectivity matrix with links between same nodes as 'con' but the
+        link weights shuffled.
+
+    """
+    # 0) SECURITY CHECKS
+    if not type(con) == numpy.ndarray:
+        raise TypeError('Please enter the connectivity matrix as a numpy array.')
+    con_shape = np.shape(con)
+    if (len(con_shape) != 2) or (con_shape[0] != con_shape[1]):
+        raise ValueError("Input not aligned. 'con' should be a 2D array of shape (N x N).")
+
+    # 1) EXTRACT INFORMATION NEEDED FROM THE con MATRIX
+    N = con_shape[0]
+
+    # Find out whether 'con' is symmetric
+    if abs(con - con.T).sum() == 0:
+        symmetric = True
+    else:
+        symmetric = False
+
+    # Find out whether 'con' is directed and calculate the number of links
+    if Reciprocity(con) == 1.0:
+        directed = False
+        L = int( round(0.5*con.astype(bool).sum()) )
+    else:
+        directed = True
+        L = con.astype(bool).sum()
+
+    # Find out whether 'con' allows self-loops (non-zero diagonal elements)
+    if con.trace() == 0:
+        selfloops = False
+    else:
+        selfloops = True
+
+    # Get the weights, as a 1D array
+    if symmetric:
+        nzidx = np.triu(con, k=1).nonzero()
+        weights = con[nzidx]
+    else:
+        nzidx = con.nonzero()
+        weights = con[nzidx]
+
+    # 2) GENERATE THE NEW NETWORK WITH THE WEIGHTS SHUFFLED
+    # Initialise the matrix. Give same dtype as 'con'
+    rewcon = np.zeros((N,N), dtype=con.dtype)
+
+    # Shuffle the list of weights
+    numpy.random.shuffle(weights)
+
+    # Finally, add the links at random
+    counter = 0
+    while counter < L:
+        # 2.1) Pick up two nodes at random
+        source = int(N * numpy.random.rand())
+        target = int(N * numpy.random.rand())
+
+        # 2.2) Check if they can be linked, otherwise look for another pair
+        if rewcon[source,target]: continue
+        if source == target and not selfloops: continue
+
+        # 2.3) Perform the rewiring
+        rewcon[source,target] = weights[counter]
+        if not directed and symmetric:
+            rewcon[target,source] = weights[counter]
+        elif not directed and not symmetric:
+            rewcon[target,source] = weights[-(counter+1)]
+        counter += 1
+
+    return rewcon
 
 
 
