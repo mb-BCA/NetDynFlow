0a5b1f65de9f1a81890b6bbaae78bd574eeb45f2
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2022-08-06 18:40:21 +0200

    Added NNt2tNN and tNN2NNt functions to tools.py.

--- netdynflow/tools.py
+++ netdynflow/tools.py
@@ -11,38 +11,102 @@
 """
 Miscelaneous tools and helpers
 ==============================
-Several extra functions to help in the analysis of networks using the dynamic
-communicability or flow
-Randomization of (weighted) networks
--------------------------------------
-RewireLinkWeights
-    Randomly re-allocates the link weights of an input network.
-RandomiseWeightedNetwork:
-    Randomises a (weighted) connectivity matrix.
-!!!!!!!!!!!!
-GORKA: Shall we may name this module as 'benchmarks' or 'nullmodels'?
-!!!!!!!!!!!!
-GORKA: I am thinking to import pyGAlib for some utilities and network generation
-functions. On the one hand, I don't want to enforce NetDynFlow users to use
-pyGAlib, just because it is my library. But on the other hand, I don't think
-that copy/pasting functions from pyGAlib into this module is robust. Replicating
-functions may lead to bugs. Better to have only one function to do a job and,
-if that one is broken, it only needs to be fixed once.
-What shall we do?
-!!!!!!!!!!!!
+
+Module for any extra helper functionality that doesn't fit as core, metric
+or network generator categories.
+
+NNt2tNN
+    Transposes a 3D array from shape (N,N,nt) to (nt,N,N) shape.
+tNN2NNt
+    Transposes a 3D array from shape (nt,N,N) to (N,N,nt) shape.
+Reciprocity
+    Computes the fraction of reciprocal links in a graph.
+
 """
 # Standard library imports
 
 # Third party packages
 import numpy as np
 import numpy.random
-# WARNING! Numba is not listed in 'requirements.txt'
-from numba import jit
 
 
 ## MISCELLANEOUS FUNCTIONS #####################################################
+def NNt2tNN(tensor):
+    """Transposes a 3D array from shape (N,N,nt) to (nt,N,N),
+
+    where N is the number of nodes in a network (features) and nt is the
+    number of time-points (samples).
+
+    Parameters
+    ----------
+    tensor : ndarray of rank-3.
+        Temporal evolution of the N x N elements in a matrix, arranged with
+        shape (N,N,nt).
+
+    Returns
+    -------
+    newtensor : ndarray of rank-3.
+        Same as input 'tensor' but in shape (nt,N,N). Matrix rows an columns at
+        each slice in 'tensor' are conserved row and columns in 'newtensor'.
+
+    Notes
+    -----
+    Please remind that np.transpose() function returns a view of the array,
+    not a copy! If you want a copy with the entries properly sorted in memory,
+    call the function as follows:
+
+    >>> arr2 = np.copy( NNt2tNN(arr1), order='C' )
+
+    """
+    # Security checks
+    assert len(np.shape(tensor)) == 3, "3D array required."
+    n0, n1, n2 = np.shape(tensor)
+    if n0 != n1:
+        raise TypeError("3D array of shape (N,N,nt) required.")
+
+    # Transpose the array
+    newtensor = np.transpose(tensor, axes=(2,0,1))
+    return newtensor
+
+def tNN2NNt(tensor):
+    """Transposes a 3D array from shape (nt,N,N) to (N,N,nt),
+
+    where N is the number of nodes in a network (features) and nt is the
+    number of time-points (samples).
+
+    Parameters
+    ----------
+    tensor : ndarray of rank-3.
+        Temporal evolution of the N x N elements in a matrix, arranged with
+        shape (nt,N,N).
+
+    Returns
+    -------
+    newtensor : ndarray of rank-3.
+        Same as input 'tensor' but in shape (N,N,nt). Matrix rows an columns at
+        each slice in 'tensor' are conserved row and columns in 'newtensor'.
+
+    Notes
+    -----
+    Please remind that np.transpose() function returns a view of the array,
+    not a copy! If you want a copy with the entries properly sorted in memory,
+    call the function as follows:
+
+    >>> arr2 = np.copy( tNN2NNt(arr1), order='C' )
+
+    """
+    # Security checks
+    assert len(np.shape(tensor)) == 3, "3D array required."
+    n0, n1, n2 = np.shape(tensor)
+    if n1 != n2:
+        raise TypeError("3D array of shape (nt,N,N) required.")
+
+    newtensor = np.transpose(tensor, axes=(1,2,0))
+    return newtensor
+
 def Reciprocity(adjmatrix):
     """Computes the fraction of reciprocal links to total number of links.
+    
     Both weighted and unweighted input matrices are permitted. Weights
     are ignored for the calculation.
     Parameters
