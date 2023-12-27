150810c107f8ba84d0b9ec9b58a52de126b355c7
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2023-12-27 22:25:48 +0100

    Cleanup of core.py
    I removed the older functions
    - CalcTensor()
    - DynFlow()
    - FullFlow()
    - IntrinsicFlow()

--- netdynflow/core.py
+++ netdynflow/core.py
@@ -37,6 +37,9 @@ RespMatrices_ContCascade
     Computes the pair-wise responses over time for the continuous cascade model.
 RespMatrices_LeakyCascade
     Computes the pair-wise responses over time for the leaky-cascade model.
+RespMatrices_ContinuousDiffusion
+    Computes the pair-wise responses over time for the linear diffusive model.
+
 
 Reference and Citation
 ----------------------
@@ -562,7 +565,7 @@ def RespMatrices_LeakyCascade(con, tau, sigma=None, tmax=10, timestep=0.1,
 
 def RespMatrices_ContinuousDiffusion(con, sigma=None, tmax=10, timestep=0.1,
                                                 case='regressed', normed=False):
-    """Computes the pair-wise responses over time for the leaky-cascade model.
+    """Computes the pair-wise responses over time for the linear diffusive model.
 
     TODO: DECIDE A BETTER NAME. WILL DEPEND ON HOW TO NAME THE FUNCTIONS FOR THE
     OTHER CANONICAL MODELS. TRY GIVE SHORTER NAMES.
@@ -674,211 +677,6 @@ def RespMatrices_ContinuousDiffusion(con, sigma=None, tmax=10, timestep=0.1,
 
     return resp_matrices
 
-
-
-
-def CalcTensor(con, tau, sigma, tmax=10, timestep=0.1,
-                                                normed=False, case='DynFlow'):
-    """Generic function to create time evolution of the flows.
-
-    DEPRECATED FUNCTION: USE RespMatrices_LeakyCascade() INSTEAD.
-    REMOVE BEFORE RELEASE OF v2.0.0
-
-    Parameters
-    ----------
-    con : ndarray of rank-2
-        The adjacency matrix of the network.
-    tau : real valued number, or ndarray of rank-1
-        The decay rate at the nodes. Positive value expected.
-        If a number is given, then the function considers all nodes have same
-        decay rate. Alternatively, an array can be inputed with the decay rate
-        of each node.
-    sigma : ndarray of rank-2
-        The covariance matrix of fluctuating inputs.
-    tmax : real valued number, positive (optional)
-        Final time for integration.
-    timestep : real valued number, positive (optional)
-        Sampling time-step.
-        Warning: Not an integration step, just the desired sampling rate.
-    normed : boolean (optional)
-        If True, normalises the tensor by the scaling factor, to make networks
-        of different size comparable.
-
-    Returns
-    -------
-    flow_tensor : ndarray of rank-3
-        Temporal evolution of the network's dynamic flow. A tensor of shape
-        (nt,N,N), where N is the number of nodes and nt = tmax * timestep is
-        the number of time steps.
-    """
-    # 0) SECURITY CHECKS
-    caselist = ['DynFlow', 'FullFlow', 'IntrinsicFlow']
-    if case not in caselist:
-        raise ValueError( "Please enter one of accepted cases: %s" %str(caselist) )
-
-    if tmax <= 0.0: raise ValueError("'tmax' must be positive")
-    if timestep <= 0.0: raise ValueError( "'timestep' must be positive")
-    if timestep > tmax: raise ValueError("Incompatible values, timestep < tmax given")
-
-    # 1) CALCULATE THE JACOBIAN MATRIX
-    jac = JacobianMOU(con, tau)
-    jacdiag = np.diagonal(jac)
-    N = len(jac)
-
-    # 2) CALCULATE THE DYNAMIC FLOW
-    # 2.1) Calculate the extrinsic flow over integration time
-    nt = int(tmax / timestep) + 1
-    sigma_sqrt = scipy.linalg.sqrtm(sigma)
-
-    flow_tensor = np.zeros((nt,N,N), dtype=np.float)
-
-    if case == 'DynFlow':
-        for i_t in range(nt):
-            t = i_t * timestep
-            # Calculate the term for jacdiag without using expm(), to speed up
-            jacdiag_t = np.diag( np.exp(jacdiag * t) )
-            # Calculate the jaccobian at given time
-            jac_t = scipy.linalg.expm(jac * t)
-            # Calculate the dynamic communicability at time t
-            flow_tensor[i_t] = np.dot( sigma_sqrt, jac_t - jacdiag_t )
-
-    elif case == 'IntrinsicFlow':
-        for i_t in range(nt):
-            t = i_t * timestep
-            # Calculate the term for jacdiag without using expm(), to speed up
-            jacdiag_t = np.diag( np.exp(jacdiag * t) )
-            # Calculate the dynamic communicability at time t.
-            flow_tensor[i_t] = np.dot( sigma_sqrt, jacdiag_t)
-
-    elif case == 'FullFlow':
-        for i_t in range(nt):
-            t = i_t * timestep
-            # Calculate the jaccobian at given time
-            jac_t = scipy.linalg.expm(jac * t)
-            # Calculate the non-normalised flow at time t.
-            flow_tensor[i_t] = np.dot( sigma_sqrt, jac_t )
-
-    # 2.2) Normalise by the scaling factor
-    if normed:
-        scaling_factor = np.abs(1./jacdiag).sum()
-        flow_tensor /= scaling_factor
-
-    return flow_tensor
-
-
-## Wrappers using CalcTensor() ___________________________________________
-def DynFlow(con, tau, sigma, tmax=10, timestep=0.1, normed=False):
-    """Pair-wise conditional flows on a network over time for a given input.
-
-    DEPRECATED FUNCTION: USE RespMatrices_LeakyCascade() INSTEAD.
-    REMOVE BEFORE RELEASE OF v2.0.0
-
-    Parameters
-    ----------
-    con : ndarray of rank-2
-        The adjacency matrix of the network.
-    tau : real valued number, or ndarray of rank-1
-        The decay rate at the nodes. Positive value expected.
-        If a number is given, then the function considers all nodes have same
-        decay rate. Alternatively, an array can be inputed with the decay rate
-        of each node.
-    sigma : ndarray of rank-2
-        The covariance matrix of fluctuating inputs.
-    tmax : real valued number, positive (optional)
-        Final time for integration.
-    timestep : real valued number, positive (optional)
-        Sampling time-step.
-        Warning: Not an integration step, just the desired sampling rate.
-    normed : boolean (optional)
-        If True, normalises the tensor by the scaling factor, to make networks
-        of different size comparable.
-
-    Returns
-    -------
-    dynflow_tensor : ndarray of rank-3
-        Temporal evolution of the network's pair-wise conditional flows.
-        A tensor of shape (nt,N,N), where N is the number of nodes and
-        nt = tmax * timestep is the number of time steps.
-    """
-    dynflow_tensor = CalcTensor(con, tau, sigma, tmax=tmax,
-                    timestep=timestep, normed=normed, case='DynFlow')
-
-    return dynflow_tensor
-
-def IntrinsicFlow(con, tau, sigma, tmax=10, timestep=0.1, normed=False):
-    """Returns the flow dissipated through each node over time.
-
-    DEPRECATED FUNCTION: USE RespMatrices_LeakyCascade() INSTEAD.
-    REMOVE BEFORE RELEASE OF v2.0.0
-
-    Parameters
-    ----------
-    con : ndarray of rank-2
-        The adjacency matrix of the network.
-    tau : real valued number, or ndarray of rank-1
-        The decay rate at the nodes. Positive value expected.
-        If a number is given, then the function considers all nodes have same
-        decay rate. Alternatively, an array can be inputed with the decay rate
-        of each node.
-    sigma : ndarray of rank-2
-        The matrix of Gaussian noise covariances.
-    tmax : real valued number, positive (optional)
-        Final time for integration.dynflow
-    timestep : real valued number, positive (optional)
-        Sampling time-step.
-        Warning: Not an integration step, just the desired sampling rate.
-    normed : boolean (optional)
-        If True, normalises the tensor by the scaling factor, to make networks
-        of different size comparable.
-
-    Returns
-    -------
-    flow_tensor : ndarray of rank-3
-        Temporal evolution of disspation through the nodes of the network.
-        A tensor of shape (nt,N,N), where N is the number of nodes and
-        nt = tmax * timestep is the number of time steps.
-    """
-    flow_tensor = CalcTensor(con, tau, sigma, tmax=tmax,
-                    timestep=timestep, normed=normed, case='IntrinsicFlow')
-
-    return flow_tensor
-
-def FullFlow(con, tau, sigma, tmax=10, timestep=0.1, normed=False):
-    """Returns the complete flow on a network over time for a given input.
-
-    DEPRECATED FUNCTION: USE RespMatrices_LeakyCascade() INSTEAD.
-    REMOVE BEFORE RELEASE OF v2.0.0
-
-    Parameters
-    ----------
-    con : ndarray of rank-2
-        The adjacency matrix of the network.
-    tau : real valued number, or ndarray of rank-1
-        The decay rate at the nodes. Positive value expected.
-        If a number is given, then the function considers all nodes have same
-        decay rate. Alternatively, an array can be inputed with the decay rate
-        of each node.
-    sigma : ndarray of rank-2
-        The matrix of Gaussian noise covariances.
-    tmax : real valued number, positive (optional)
-        Final time for integration.
-    timestep : real valued number, positive (optional)
-        Sampling time-step. NOT an integration step, but the sampling step.
-    normed : boolean (optional)
-        If True, normalises the tensor by the scaling factor, to make networks
-        of different size comparable.
-
-    Returns
-    -------
-    flow_tensor : ndarray of rank-3
-        Temporal evolution of the network's full-flow. A tensor of shape
-        (nt,N,N), where N is the number of nodes and nt = tmax * timestep is
-        the number of time steps.
-    """
-    flow_tensor = CalcTensor(con, tau, sigma, tmax=tmax,
-                            timestep=timestep, normed=normed, case='FullFlow')
-
-    return flow_tensor
 
 
 ##
