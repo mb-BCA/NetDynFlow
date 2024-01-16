c5041c2e27f797d07214005a0b7506339d28e9da
Gorka Zamora-López <gorka@zamora-lopez.xyz>
2024-01-16 21:01:17 +0100

    Added ContinuousCascade()

--- netdynflow/simulate_numba.py
+++ netdynflow/simulate_numba.py
@@ -315,76 +315,99 @@ def LeakyCascade(con, X0, tau, noise=None, tmax=10, timestep=0.01):
     # 2) RUN THE SIMULATION
     return IntegrationLoop()
 
-# def ContinuousDiffusion(con, X0, tmax=10, timestep=0.01, noise=None):
-#     """Simulates the temporal evolution of the nodes for the continuous diffusion.
-#
-#     It solves the differential equation for the simplest possible linear
-#     dynamical (propagation) linear model of nodes coupled via diffusive coupling,
-#     with no local dynamics at the nodes.
-#
-#         xdot_i = A_ij * (x_i - x_j) .
-#
-#     In matrix form, this equation is represented as:
-#
-#         xdot(t) = -D x(t) + A x(t)  =  L x(t),
-#
-#     where D is the diagonal matrix with the input degrees ink_i in the diagonal
-#     and L = -D + A is the graph Laplacian matrix.
-#
-#     Parameters
-#     ----------
-#     con : ndarray of rank-2, and shape (N,N).
-#         The adjacency matrix of the network.
-#     X0 : ndarray of rank-1, and length N.
-#         The initial conditions. Entries X0[i] are the inital values of the nodes.
-#         They can be either positive or negative, real or integer values.
-#     tmax : scalar (optional)
-#         The duration of the simulation in arbitrary time units.
-#     timestep : scalar (optional)
-#         The time-step of the numerical integration.
-#     noise : ndarray of rank-2 (optional)
-#         A precomputed noisy input to all nodes. Optional parameter. Not needed
-#         for the applications of the model-based network analysis. Left optional
-#         for general purposes. If given, 'noise' shall be a numpy array of shape
-#         (nsteps, N), where nsteps = int(tmax*timestep) + 1 and N is the number of nodes.
-#
-#     Returns
-#     -------
-#     Xdot : ndarray of rank-2
-#         Time-courses of the N nodes. A numpy array of shape (nsteps, N),
-#         where nsteps = int(tmax*timestep) + 1.
-#     """
-#     # 0) SECURITY CHECKS
-#     # To be done ...
-#
-#     # 1) PREPARE FOR THE SIMULATION
-#     # Infos about the network
-#     N = len(con)
-#     conT = np.copy(con.T, order='C')
-#     ink = conT.sum(axis=1)
-#     # Lmat = - ink * np.identity(N, dtype=np.float64) + conT
-#
-#     # Initialise the output array
-#     nsteps = int(tmax / timestep) + 1
-#     Xdot = np.zeros((nsteps, N), np.float64, order='C')
-#     # Enter the initial conditions
-#     Xdot[0] = X0
-#
-#     # Set the noise array to default, if not given by the user
-#     if noise is None: noise = np.zeros((nsteps,N), dtype=np.float64)
-#
-#     # 2) RUN THE SIMULATION
-#     for t in range(1,nsteps):
-#         Xpre = Xdot[t-1]
-#         # Calculate the input to nodes due to couplings
-#         xcoup = np.dot(conT,Xpre) - ink * Xpre
-#         # xcoup = np.dot(Lmat, Xpre)
-#         # Integration step
-#         # Xdot[t] = Xpre + timestep * ( gcoupling*xcoup ) + noise[t]
-#         Xdot[t] = Xpre + timestep * xcoup + noise[t]
-#
-#     return Xdot
+def ContinuousDiffusion(con, X0, noise=None, tmax=10, timestep=0.01):
+    """Simulates the temporal evolution of the nodes for the continuous diffusion.
 
+    It solves the differential equation for the simplest possible linear
+    dynamical (propagation) linear model of nodes coupled via diffusive coupling,
+    with no local dynamics at the nodes.
+
+        xdot_i = A_ij * (x_i - x_j) .
+
+    In matrix form, this equation is represented as:
+
+        xdot(t) = -D x(t) + A x(t)  =  L x(t),
+
+    where D is the diagonal matrix with the input degrees ink_i in the diagonal
+    and L = -D + A is the graph Laplacian matrix.
+
+    Parameters
+    ----------
+    con : ndarray of rank-2, and shape (N,N).
+        The adjacency matrix of the network.
+    X0 : ndarray of rank-1, and length N.
+        The initial conditions. Entries X0[i] are the inital values of the nodes.
+        They can be either positive or negative, real or integer values.
+    noise : ndarray of rank-2 (optional)
+        A precomputed noisy input to all nodes. Optional parameter. Not needed
+        for the applications of the model-based network analysis. Left optional
+        for general purposes. If given, 'noise' shall be a numpy array of shape
+        (nsteps, N), where nsteps = int(tmax*timestep) + 1 and N is the number of nodes.
+    tmax : scalar (optional)
+        The duration of the simulation in arbitrary time units.
+    timestep : scalar (optional)
+        The time-step of the numerical integration.
+
+    Returns
+    -------
+    Xdot : ndarray of rank-2
+        Time-courses of the N nodes. A numpy array of shape (nsteps, N),
+        where nsteps = int(tmax*timestep) + 1.
+    """
+    @jit(nopython=True)
+    def IntegrationLoop():
+        # Initialise the output array
+        Xdot = np.zeros((nsteps, N), np.float64)
+        # Enter the initial conditions
+        Xdot[0] = X0
+
+        # 2) RUN THE SIMULATION
+        for t in range(1,nsteps):
+            Xpre = Xdot[t-1]
+            # Calculate the input to nodes due to couplings
+            xcoup = np.dot(conT,Xpre) - ink * Xpre
+            # xcoup = np.dot(Lmat, Xpre)
+            # Integration step
+            Xdot[t] = Xpre + timestep * xcoup + noise[t]
+
+        return Xdot
+
+    # 0) SECURITY CHECKS AND HANDLE THE INPUTS
+    # Check the times
+    if not isinstance(tmax, numbers.Number):
+        raise ValueError( "'tmax' should be a number" )
+    if not isinstance(timestep, numbers.Number):
+        raise ValueError( "'timestep' should be a number" )
+    # Calculate the simulation length
+    nsteps = int(tmax / timestep) + 1
+
+    # Check the connectivity matrix
+    N = len(con)
+
+    # Handle the noise input.
+    if noise is None:
+        # If nothing given by user, set noise to zeros.
+        noise = np.zeros((nsteps,N), np.float64)
+    elif isinstance(noise, np.ndarray): pass
+    elif isinstance(noise, (list, tuple)):
+        # If array-like, convert to ndarray
+        noise = np.array(noise, np.float64)
+    else:
+        raise TypeError( "'noise' should be either None or a 2D array-like of shape (nsteps,N)." )
+
+    # Ensure all arrays are of same dtype (np.float64)
+    if con.dtype != np.float64:    con = con.astype(np.float64)
+    if X0.dtype != np.float64:     X0 = X0.astype(np.float64)
+    if noise.dtype != np.float64:  noise = noise.astype(np.float64)
+
+    # 1) PREPARE FOR THE SIMULATION
+    # Transpose the connectivity matrix
+    conT = np.copy(con.T, order='C')
+    ink = conT.sum(axis=1)
+    # Lmat = - ink * np.identity(N, dtype=np.float64) + conT
+
+    # 2) RUN THE SIMULATION
+    return IntegrationLoop()
 
 
 
